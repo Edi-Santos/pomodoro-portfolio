f392fbabcb5f14167a6cca9431091651902dd9c2
Ed Santos <edsantos2822@gmail.com>
2022-07-22 18:09:05 -0300

    chore:(clock.js): alterando nome da função

--- clock.js
+++ clock.js
@@ -1,6 +1,6 @@
 const clock = document.querySelector('.clock');
 
-function timer() {
+function clock() {
   let hours = new Date().getHours();
   let minutes = new Date().getMinutes();
   let seconds = new Date().getSeconds();
@@ -12,4 +12,4 @@ function timer() {
   clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
-setInterval(timer, 1000);
+setInterval(clock, 1000);
