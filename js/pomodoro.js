9fe753ca4bf7297a04a149db77518a58c24abb61
Ed Santos <edsantos2822@gmail.com>
2022-08-01 17:39:00 -0300

    feat: criando arquivo JS e temporizador pomodoro para trabalho

---
+++ js/pomodoro.js
@@ -0,0 +1,22 @@
+const workTimer = document.querySelector('.work-timer');
+const start = document.querySelector('.start');
+
+let minutes = 25;
+let seconds = 00;
+
+function workTime() {
+  if (seconds >= 0 && seconds < 60 && minutes >= 0) {
+    if (seconds === 0) {
+      seconds = 59;
+      minutes -= 1;
+    } else {
+      seconds -= 1;
+    } 
+  }
+
+  workTimer.innerText = `${minutes}:${seconds}`;
+}
+
+start.addEventListener('click', () => {
+  const interval = setInterval(() => workTime(), 1000);
+})
