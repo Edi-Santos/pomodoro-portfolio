0846d0321ade539a0e4f4c5dbf290efa88f051bf
Ed Santos <edsantos2822@gmail.com>
2022-08-02 21:48:49 -0300

    build:(pomodoro.html): adicionando título ao temporizador

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,14 +1,39 @@
 const workTimer = document.querySelector('.work-timer');
+const restTimer = document.querySelector('.rest-timer');
 const restart = document.querySelector('.restart');
 const start = document.querySelector('.start');
 const stop = document.querySelector('.stop');
 
 let minutes = 25;
+let restMin = 5;
 let seconds = 00;
+let restSec = 00;
+
+function restTime() {
+  restTimer.innerText = '5:00';
+
+  if (restSec >= 0 && restSec < 60 && restMin >= 0) {
+    if (restSec === 0 && restMin === 0) return restTimer.innerText = '0:0';
+    if (restSec === 0 && restMin > 0) {
+      restSec = 59;
+      restMin -= 1;
+    } else {
+      restSec -= 1;
+    }
+  }
+
+  restTimer.innerText = `${restMin}:${restSec}`;
+}
 
 function workTime() {
   if (seconds >= 0 && seconds < 60 && minutes >= 0) {
-    if (seconds === 0) {
+    if (seconds === 0 && minutes === 0) {
+      workTimer.innerText = '';
+
+      return restTime();
+    }
+
+    if (seconds === 0 && minutes > 0) {
       seconds = 59;
       minutes -= 1;
     } else {
@@ -27,6 +52,7 @@ start.addEventListener('click', () => {
   restart.addEventListener('click', () => {
     clearInterval(interval);
 
+    restTimer.innerText = '';
     workTimer.innerText = '25:00';
   });
 })
