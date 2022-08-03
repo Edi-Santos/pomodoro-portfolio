96a9c9afcf2706d9563f7a9aa1b80159923cca39
Ed Santos <edsantos2822@gmail.com>
2022-08-03 20:52:55 -0300

    test: configurando cypress e adicionndo o primeiro arquivo de testes

---
+++ cypress/e2e/index.cy.js
@@ -0,0 +1,8 @@
+const HEADER = 'header';
+
+describe('Testando página de Relógio', () => {
+  it('testa se há um <header />', () => {
+    cy.visit('http://127.0.0.1:5500/pages/index/');
+  });
+});
+  
