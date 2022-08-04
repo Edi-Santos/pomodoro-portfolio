4259fca15c7ccb20b3d63897f73ef468dee01d52
Ed Santos <edsantos2822@gmail.com>
2022-08-03 21:33:16 -0300

    test:(index.cy.js): implementando o teste 1

--- cypress/e2e/index.cy.js
+++ cypress/e2e/index.cy.js
@@ -1,8 +1,10 @@
 const HEADER = 'header';
+const HEADER_NAV = 'header nav';
 
 describe('Testando página de Relógio', () => {
-  it('testa se há um <header />', () => {
+  it('1 - testa se há um <header />', () => {
     cy.visit('http://127.0.0.1:5500/pages/index/');
+    cy.get(HEADER).should('exist');
   });
 });
   
