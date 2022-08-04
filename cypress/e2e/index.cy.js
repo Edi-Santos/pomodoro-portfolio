e7f5d047034aff135c4e814ef212e07aa9fe261f
Ed Santos <edsantos2822@gmail.com>
2022-08-03 21:49:40 -0300

    test:(index.cy.js): implementando o teste 3

--- cypress/e2e/index.cy.js
+++ cypress/e2e/index.cy.js
@@ -1,5 +1,6 @@
 const HEADER = 'header';
 const HEADER_NAV = 'header nav';
+const MAIN = 'main';
 
 describe('Testando página de Relógio', () => {
   it('1 - testa se há um <header />', () => {
@@ -10,6 +11,10 @@ describe('Testando página de Relógio', () => {
   it('2 - Testa se no <header /> há um <nav />, e se, na TAG <nav />, há 3 elementos filhos', () => {
     cy.get(HEADER_NAV).should('exist');
     cy.get(HEADER_NAV).children().should('have.length', 3);
+  });
+
+  it('3 - Testa se há um <main />', () => {
+    cy.get(MAIN).should('exist');
   });
 });
   
