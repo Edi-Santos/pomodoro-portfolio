588769ae412895f8213c35b0d8828fadb47528fa
Ed Santos <edsantos2822@gmail.com>
2022-08-04 10:29:00 -0300

    test:(index.cy.js): implementando o teste 7

--- cypress/e2e/chronometer.cy.js
+++ cypress/e2e/chronometer.cy.js
@@ -1,5 +1,6 @@
 const HEADER = 'header';
 const HEADER_NAV = 'header nav';
+const MAIN = 'main';
 
 describe('Testando página de Cronômetro', () => {
   it('5 - testa se há um <header />', () => {
@@ -10,5 +11,9 @@ describe('Testando página de Cronômetro', () => {
   it('6 - Testa se no <header /> há um <nav />, e se, na TAG <nav />, há 3 elementos filhos', () => {
     cy.get(HEADER_NAV).should('exist');
     cy.get(HEADER_NAV).children().should('have.length', 3);
+  });
+
+  it('7 - Testa se há um <main />', () => {
+    cy.get(MAIN).should('exist');
   });
 });
