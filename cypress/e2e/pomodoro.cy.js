9068df7d84ee5030048662a064eb701cdf87efeb
Ed Santos <edsantos2822@gmail.com>
2022-08-05 23:35:59 -0300

    test:(pomodoro.cy.js): implementando o teste 17

--- cypress/e2e/pomodoro.cy.js
+++ cypress/e2e/pomodoro.cy.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 
 const HEADER = 'header';
+const HEADER_NAV = 'header nav';
 
 describe('Testa página Pomodoro', () => {
   before(() => {
@@ -9,5 +10,10 @@ describe('Testa página Pomodoro', () => {
 
   it('16 - testa se há um <header />', () => {
     cy.get(HEADER).should('exist');
+  });
+
+  it('17 - testa se no <header /> há um <nav />, e se, na TAG <nav />, há 3 elementos filhos', () => {
+    cy.get(HEADER_NAV).should('exist');
+    cy.get(HEADER_NAV).children().should('have.length', 3);
   });
 });
