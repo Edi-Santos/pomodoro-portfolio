dfd75ef8ab3b400c658964a7e37111ad59cd6dd4
Ed Santos <edsantos2822@gmail.com>
2022-08-08 14:56:02 -0300

    test:(chronometer.cy.js): corrigindo URL para testes

--- cypress/e2e/chronometer.cy.js
+++ cypress/e2e/chronometer.cy.js
@@ -9,7 +9,7 @@ const CLASS_BACK_TO_ZERO = '.back-to-zero';
 
 describe('Testando página de Cronômetro', () => {
   it('5 - testa se há um <header />', () => {
-    cy.visit('http://127.0.0.1:5500/pages/chronometer/chronometer.html');
+    cy.visit('http://127.0.0.1:5500/pages/timer/timer.html');
     cy.get(HEADER).should('exist');
   })
 
